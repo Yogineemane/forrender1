b5736c4b1181c5becc0d7cc2ef0ef871fe86fe5f
Yogineemane <157476176+Yogineemane@users.noreply.github.com>
2024-04-26 02:11:43 -0700

    first upload

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send("HELLO")
+})
+
+app.listen(process.env.PORT || 5000)
+
+async function mongoConnection() {
+   try {
+    const res = await mongoose.connect(process.env.DB)
+    const data = await res.default
+    console.log(data.STATES.connected);
+   } catch (error) {
+    console.log("Error",error.message);
+   }
+} 
+mongoConnection()
