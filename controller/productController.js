767153906b4d98795bf5b7d6de84b096f2787c31
Yogineemane <157476176+Yogineemane@users.noreply.github.com>
2024-04-26 03:30:12 -0700

    added some functions in route and controller

--- controller/productController.js
+++ controller/productController.js
@@ -8,3 +8,29 @@ exports.getProduct = async (req,res)=>{
         return res.status(400).json({errors:true,message:error.message})
     }
 }
+exports.postProduct = async (req,res)=>{
+    try {
+        const data = await Product.create(req.body)
+        return res.json({errors:false,data:data})
+    } catch (error) {
+        return res.status(400).json({errors:true, message:error.message})
+    }
+}
+
+exports.putProduct = async (req,res)=>{
+    try {
+        const data = await Product.findByIdAndUpdate(req.params.id,req.body,{new:true})
+        return res.json({errors:false,data:data})
+    } catch (error) {
+        return res.status(400).json({errors:true, message:error.message})
+    }
+}
+
+exports.deleteProduct = async (req,res)=>{
+    try {
+        const data = await Product.findByIdAndDelete(req.param.id)
+        return res.json({errors:false,data:data})
+    } catch (error) {
+        return res.status(400).json({errors:true, message:error.message})
+    }
+}
--- route/productRoute.js
+++ route/productRoute.js
@@ -1,7 +1,10 @@
 const route = require('express').Router()
 
-const {getProduct} = require('../controller/productController')
+const {getProduct,postProduct,putProduct,deleteProduct} = require('../controller/productController')
 
 route.get('/',getProduct)
+route.post('/',postProduct)
+route.put('/:id',putProduct)
+route.delete('/:id',deleteProduct)
 
 module.exports = route 
